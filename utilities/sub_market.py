dab247d3e53c753c0d2362b47e28676f1c5467da
Lynn <hlxstc680@gmail.com>
2020-04-14 10:39:28 +0800

    refactor:

--- utilities/sub_market.py
+++ utilities/sub_market.py
@@ -5,7 +5,6 @@ import time
 from decimal import Decimal
 
 import requests
-from pytz import timezone
 from websocket import create_connection
 from utilities.tools import inflate
 from datetime import datetime
@@ -44,7 +43,7 @@ class SubscribeHUOBIFuture(threading.Thread):
     def __init__(self, name):
         super().__init__(name=name)
         self.timestamp = None
-        self.close = None
+        self.close: Decimal = None
         self.ws_url = create_connection("wss://www.hbdm.com/ws")
 
     def run(self):
@@ -55,32 +54,6 @@ class SubscribeHUOBIFuture(threading.Thread):
                 pass
             elif 'ping' in json_obj.keys():
                 pong = '{"pong":'+str(json_obj['ping'])+'}'
-                self.ws_url.send(pong)
-            elif 'ch' in json_obj.keys():
-                tick = json_obj['tick']
-                timestamp = datetime.utcfromtimestamp(
-                    tick['id'])
-                self.timestamp = timestamp
-                self.close = tick['close']
-
-
-class SubscribeHUOBI(threading.Thread):
-
-    def __init__(self, name):
-        super().__init__(name=name)
-        self.timestamp = None
-        self.close = None
-        self.ws_url = create_connection("wss://api.huobi.pro/ws")
-
-    def run(self):
-        while True:
-            result = gzip.decompress(self.ws_url.recv()).decode('utf-8')
-            json_obj = json.loads(result)
-            if json_obj.get('status') == 'error':
-                pass
-            elif 'ping' in json_obj.keys():
-                pong = '{"pong":'+str(json_obj['ping'])+'}'
-
                 self.ws_url.send(pong)
             elif 'ch' in json_obj.keys():
                 tick = json_obj['tick']
@@ -102,9 +75,9 @@ class SubscribeHUOBI(Subscribe):
 
     def __init__(self, base_symbol, quote_symbol, name='HUOBI'):
         super().__init__(name=name)
-        self.ws_url = create_connection("wss://www.hbdm.com/ws")
+        self.ws_url = create_connection("wss://api.huobi.pro/ws")
         topic = {
-            "sub": f"market.{base_symbol}{quote_symbol}.kline.1min",
+            "sub": f"market.{str.lower(base_symbol)}{str.lower(quote_symbol)}.kline.1min",
             "id": "id1"
         }
         self.ws_url.send(json.dumps(topic))
@@ -121,26 +94,26 @@ class SubscribeHUOBI(Subscribe):
                 self.ws_url.send(pong)
             elif 'ch' in json_obj.keys():
                 tick = json_obj['tick']
-                timestamp = datetime.utcfromtimestamp(
-                    tick['id'])
-                self.timestamp = timestamp
-                self.close = tick['close']
+                self.timestamp = tick['id']
+                self.close = Decimal(str(tick['close']))
 
 
 class SubscribeEXMO(Subscribe):
 
     def __init__(self, base_symbol, quote_symbol, name='EXMO'):
         super().__init__(name=name)
-        self.ticker_url = create_connection("https://api.exmo.com/v1/ticker/")
+        self.ticker_url = "https://api.exmo.com/v1/ticker/"
         self.base_symbol = base_symbol
         self.quote_symbol = quote_symbol
 
     def run(self):
         while True:
             time.sleep(0.5)
-
-            res = requests.get(self.ticker_url)
-            ticker = json.loads(res.content)[f'{self.base_symbol}_{self.quote_symbol}']
+            try:
+                res = requests.get(self.ticker_url, timeout=5)
+            except requests.exceptions.ProxyError or requests.exceptions.SSLError or requests.exceptions.ReadTimeout:
+                continue
+            ticker = json.loads(res.content)[f'{str.upper(self.base_symbol)}_{str.upper(self.quote_symbol)}']
             updated = ticker['updated']
 
             self.timestamp = updated
