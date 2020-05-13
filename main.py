20f98cbba4d77f0a4a34eff92ac20dc2638f2193
Lynn <hlxstc680@gmail.com>
2020-05-13 21:43:54 +0800

    refacotr

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ os.environ['HTTPS_PROXY'] = "https://127.0.0.1:1082"
 
 def get_huobi_future_data():
     base_time = datetime.strptime('2020-05-13', '%Y-%m-%d')
-    last_year = base_time - timedelta(days=30)
+    last_year = base_time - timedelta(days=7)
     begin = int(datetime.timestamp(last_year))
     end = int(datetime.timestamp(base_time))
     to = begin + 2000 * 60
@@ -24,24 +24,32 @@ def get_huobi_future_data():
     if begin and end:
         while to < end:
             print(f'{begin}----{to}')
-            response_data = history.req_data_by_time_range(begin, to)
+            try:
+                response_data = history.req_data_by_time_range(begin, to)
+            except Exception as e:
+                print(e)
+                continue
             bars.extend(history.construct(response_data))
             begin = to
             to += 2000 * 60
             time.sleep(1)
 
     df = pd.DataFrame(data=[bar.__dict__ for bar in bars])
-    df.to_csv('data/csv/huobi_future_test.csv')
+    df.to_csv('data/csv/huobi_future_test_7days.csv')
 
 
 if __name__ == "__main__":
 
     # get_huobi_future_data()
+
     base_symbol = 'BTC'
     quote_symbol = 'USDT'
     pos = 5
     intervals = '1min'
 
-    run = TurtleRunBackTest(base_symbol, quote_symbol, pos, intervals)
+    # run = TurtleRunBackTest(
+    #     base_symbol, quote_symbol, pos, intervals, 'data/csv/huobi_future_test.csv', '../log/results.csv')
+
+    run = TurtleRun(base_symbol, quote_symbol, pos, intervals)
     run.proceed()
 
